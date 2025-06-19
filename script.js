df415cbaa7e539f1cc17ca8fae35fd87ca18625b
choski91 <mlaura.smichowski@gmail.com>
2025-06-19 09:37:10 +0200

    validacion de formulario

---
+++ script.js
@@ -0,0 +1,46 @@
+//Validacion de formulario
+console.log("hola");
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event.target.elements);
+
+  console.log(event.target.nombre.value);
+  console.log(event.target.email.value);
+  console.log(event.target.mensaje.value);
+
+  const nombre = event.target.nombre.value;
+  const email = event.target.email.value;
+  const mensaje = event.target.mensaje.value;
+
+  let msj = "";
+
+  if (nombre.length < 3 || nombre.length > 30) {
+
+    console.log("Nombre fuera de tamaño: 3-30");
+    msj += "Nombre fuera de tamaño: 3-30\n";
+  }
+
+  if (
+    (!email.endsWith(".com") && !email.endsWith(".es")) ||
+    !email.includes("@")
+  ) {
+    console.log("Error validación: " + email);
+    msj += "Error validación: " + email + "\n";
+  }
+
+  if (msj.length != 0) {
+    alert(msj);
+    let p = document.createElement("pre");
+    let mensaje = document.createTextNode(msj);
+    p.style.color = "#DD1C1A";
+    p.style.fontSize = "16px";
+    p.appendChild(mensaje);
+
+    document.getElementById("Contacto").appendChild(p);
+  }
+  else {
+
+    alert("Formulario enviado con éxito");
+    event.target.submit();
+  }
+});
